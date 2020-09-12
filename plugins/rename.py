e7027405487ae20f3adb15ef1eec43c44297d682
JACK <69492163+Jack-tg@users.noreply.github.com>
2020-09-12 11:24:16 +0530

    Update rename.py

--- plugins/rename.py
+++ plugins/rename.py
@@ -23,7 +23,7 @@ from translation import Translation
 
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-from pyrogram import Client, Filters, InlinekeyboardMarkup, Inlinekeyboardbutton,callback_query
+from pyrogram import Client, Filters, Forcereply,callback_query
 
 from helper_funcs.chat_base import TRChatBase
 from helper_funcs.display_progress import progress_for_pyrogram
