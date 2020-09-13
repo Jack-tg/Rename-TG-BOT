b460f778cbe7232a6cca5c753f47685ddcbd03c6
JACK <69492163+Jack-tg@users.noreply.github.com>
2020-09-13 15:55:33 +0530

    Update url.py

--- plugins/url.py
+++ plugins/url.py
@@ -1,14 +1,11 @@
 import datetime
 
 from pyrogram import filters as  Filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-from ..utils import is_url, is_valid_file, get_duration, gen_ik_buttons, generate_stream_link
-from ..screenshotbot import ScreenShotBot
-from ..config import Config
 
 
-@ScreenShotBot.on_message(Filters.private & ((Filters.text & ~Filters.edited) | Filters.media) & Filters.incoming)
+
+
+@Bot.on_message(Filters.private & ((Filters.text & ~Filters.edited) | Filters.media) & Filters.incoming)
 async def _(c, m):
     
     if m.media:
@@ -19,4 +16,5 @@ async def _(c, m):
             return
 
     snt = await m.reply_text("Hi there, Please wait while I'm getting everything ready to process your request!", quote=True)
+    )
     
