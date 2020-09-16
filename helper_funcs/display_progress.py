c6e7ed6a89ec00ff0da67c355b1163685ece7ea6
JACK <69492163+Jack-tg@users.noreply.github.com>
2020-09-16 18:11:05 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -19,7 +19,7 @@ else:
     from config import Config
 
 # the Strings used for this "thing"
-from script import script
+from translation import Translation
 
 
 async def progress_for_pyrogram(
@@ -42,12 +42,12 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \n\n⭕️Progress: {2}%\n".format(
-            ''.join(["▣" for i in range(math.floor(percentage / 5))]),
-            ''.join(["▢" for i in range(20 - math.floor(percentage / 5))]),
+        progress = "[{0}{1}] \nP: {2}%\n".format(
+            ''.join(["■" for i in range(math.floor(percentage / 5))]),
+            ''.join(["□" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\n\n️⭕️Speed: {2}/s\n\n⭕️ETA: {3}\n".format(
+        tmp = "**{0} of {1}**\n**Speed⚡**: {2}/s\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
