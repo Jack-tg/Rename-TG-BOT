68bcb3c288f5dcc839899992f4a0b3744d6b7c44
JACK <69492163+Jack-tg@users.noreply.github.com>
2020-09-16 19:01:12 +0530

    Update url.py

--- plugins/url.py
+++ plugins/url.py
@@ -6,14 +6,12 @@ from pyrogram import Filters,Client
 
 
 @pyrogram.Client.on_message(Filters.private & ((Filters.text & ~Filters.edited) | Filters.media) & Filters.incoming)
-async def _(c, m):
+async def _(m):
     
     if m.media:
-        if not is_valid_file(m):
+        if is_valid_file(m):
             return
-    else:
-        if not is_url(m.text):
-            return
+
 
     snt = await m.reply_text("Hi there, Please wait while I'm getting everything ready to process your request!", quote=True)
     
