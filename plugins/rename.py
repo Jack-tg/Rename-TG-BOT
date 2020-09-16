54a626358a9453ecfec1c455a7218a911f19aa16
JACK <69492163+Jack-tg@users.noreply.github.com>
2020-09-16 19:20:08 +0530

    Update rename.py

--- plugins/rename.py
+++ plugins/rename.py
@@ -37,7 +37,7 @@ from database.database import *
 @pyrogram.Client.on_message(pyrogram.Filters.document)
 async def document(bot, update):
     # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/start")
+    
     await bot.send_message(
         chat_id=update.chat.id,
         text="hi dude"
